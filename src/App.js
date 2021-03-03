38e91878ad78279799c926f29cbebdf4cc3bf711
james-julius <jamesdarby7@gmail.com>
2021-03-03 13:42:56 -0800

    Squares working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
 import './App.scss';
 import { useState } from 'react'
-import { Box, Icon } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 
 function App() {
-  const [playerOneSquares, setPlayerOneSquares] = useState([]);
-  const [playerTwoSquares, setPlayerTwoSquares] = useState([]);
+  const [currentPlayer, setCurrentPlayer] = useState(1);
   const [gameSquares, setGameSquares] = useState([
     {
       isChecked: false,
@@ -44,6 +43,7 @@ function App() {
       byPlayer: null
     }
   ]);
+
   const winningConditions = [
     [1, 2, 3],
     [4, 5, 6],
@@ -58,18 +58,80 @@ function App() {
     [7, 8, 9]
   ];
 
+  function checkIfPlayerWon(playerNum) {
+    // Check against the win conditions to see if it has been met. This time complexity
+    //  can be improved. 
+    for (let winningArr of winningConditions) {
+      // True until proven false
+      let hasWon = true;
+      for (let squareNum of gameSquares) {
+        // Does a player have each square in a winning set?
+        // Correct until proven wrong. 
+        if (!winningArr.includes(squareNum)) {
+          hasWon = false;
+        }
+      }
+      // The player won
+      if (hasWon) return true;
+    }
+    // The player has not won
+    return false;
+  }
+
+  function switchPlayer() {
+    if (currentPlayer === 1) {
+      setCurrentPlayer(2);
+    }
+    if (currentPlayer === 2) {
+      setCurrentPlayer(1);
+    }
+  }
+
+  function handleClick(squareId) {
+    console.log(squareId + ' clicked')
+    // Make sure it hasn't already been played
+    if (gameSquares[squareId].isChecked === false) {
+      // Create a shallow copy to then set as the new state
+      let newGameSquares = [...gameSquares];
+      let squareToCheck = newGameSquares[squareId];
+      squareToCheck.isChecked = true;
+      if (currentPlayer === 1) {
+        squareToCheck.byPlayer = 1;
+        checkIfPlayerWon(1);
+      }
+      if (currentPlayer === 2) {
+        squareToCheck.byPlayer = 2;
+        checkIfPlayerWon(2);
+      }
+      // Persist in setState
+      setGameSquares(newGameSquares);
+      switchPlayer();
+    }
+  }
+
   return (
     <Box className="game-page">
       <Box className="game-container">
-        {gameSquares.map((square, squareId) => (
-          <Box className="game-square" key={squareId}>
-            {square.isFilled && (
-              <Box className="check">
-                {square.byPlayer === 1 ? ("X") : ("O")}
-              </Box>
-            )}
-          </Box>
-        ))}
+        {gameSquares.map((square, squareId) => {
+          console.log(square);
+          console.log(square.byPlayer === 1)
+          return (
+            <Box
+              className="game-square"
+              key={squareId}
+              onClick={() => handleClick(squareId)}
+            >
+              {square.isChecked && (
+                <Box className="check">
+                  <Text>
+                    {square.byPlayer === 1 ? "X" : null}
+                    {square.byPlayer === 2 ? "O" : null}
+                  </Text>
+                </Box>
+              )}
+            </Box>
+          );
+        })}
       </Box>
     </Box>
   );
