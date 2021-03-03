4e84d4bfdb1707aa64e1703b29c5594bc04c0204
james-julius <jamesdarby7@gmail.com>
2021-03-03 13:15:51 -0800

    Boxes now displaying, with setup for each player and game winning conditions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import { useState } from 'react'
+import { Box, Icon } from '@chakra-ui/react'
 
 function App() {
+  const [playerOneSquares, setPlayerOneSquares] = useState([]);
+  const [playerTwoSquares, setPlayerTwoSquares] = useState([]);
+  const [gameSquares, setGameSquares] = useState([
+    {
+      isChecked: false,
+      byPlayer: null
+    },
+    {
+      isChecked: false,
+      byPlayer: null
+    },
+    {
+      isChecked: false,
+      byPlayer: null
+    },
+    {
+      isChecked: false,
+      byPlayer: null
+    },
+    {
+      isChecked: false,
+      byPlayer: null
+    },
+    {
+      isChecked: false,
+      byPlayer: null
+    },
+    {
+      isChecked: false,
+      byPlayer: null
+    },
+    {
+      isChecked: false,
+      byPlayer: null
+    },
+    {
+      isChecked: false,
+      byPlayer: null
+    }
+  ]);
+  const winningConditions = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [7, 5, 3],
+    [1, 5, 9],
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="game-page">
+      <Box className="game-container">
+        {gameSquares.map((square, squareId) => (
+          <Box className="game-square" key={squareId}>
+            {square.isFilled && (
+              <Box className="check">
+                {square.byPlayer === 1 ? ("X") : ("O")}
+              </Box>
+            )}
+          </Box>
+        ))}
+      </Box>
+    </Box>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React from "react";
+// import { extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Theme config
+// const colors = {
+//   brand: {
+//     900: "#1a365d",
+//     800: "#153e75",
+//     700: "#2a69ac",
+//   },
+// };
+// const chakraTheme = extendTheme({ colors });
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
